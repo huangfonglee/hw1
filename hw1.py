6a31156eae7e330a4ce8e069a74e921a35698a9a
huangfonglee <huang.fong.lee666@gmail.com>
2021-03-07 23:28:24 -0800

    add hw1.py

--- hw1.py
+++ hw1.py
@@ -49,8 +49,6 @@ target_data = list(filter(lambda item: (item['station_id'] == 'C0A880' or item['
 
 
 #=======================================
-
-
 # Part. 4
 
 #=======================================
@@ -64,56 +62,56 @@ TEMP_C0R190 = 0.0
 TEMP_C0X260 = 0.0
 
 for i in range(len(target_data)):
-   if (target_data[i]['station_id'] == 'C0A880') :
-      if (target_data[i]['TEMP'] == '-99.000' or target_data[i]['TEMP'] == '-999.000') :
+   if target_data[i]['station_id'] == 'C0A880' :
+      if target_data[i]['TEMP'] == '-99.000' or target_data[i]['TEMP'] == '-999.000' :
          TEMP_C0A880 = TEMP_C0A880
       else :
-         if (float(target_data[i]['TEMP']) > TEMP_C0A880):
+         if float(target_data[i]['TEMP']) > TEMP_C0A880:
             TEMP_C0A880 = float(target_data[i]['TEMP'])
-   elif (target_data[i]['station_id'] == 'C0F9A0') :
-      if (target_data[i]['TEMP'] == '-99.000' or target_data[i]['TEMP'] == '-999.000') :
+   elif target_data[i]['station_id'] == 'C0F9A0' :
+      if target_data[i]['TEMP'] == '-99.000' or target_data[i]['TEMP'] == '-999.000' :
          TEMP_C0F9A0 = TEMP_C0F9A0
       else :
-         if (float(target_data[i]['TEMP']) > TEMP_C0F9A0):
+         if float(target_data[i]['TEMP']) > TEMP_C0F9A0:
             TEMP_C0F9A0 = float(target_data[i]['TEMP'])
-   elif (target_data[i]['station_id'] == 'C0G640') :
-      if (target_data[i]['TEMP'] == '-99.000' or target_data[i]['TEMP'] == '-999.000') :
+   elif target_data[i]['station_id'] == 'C0G640' :
+      if target_data[i]['TEMP'] == '-99.000' or target_data[i]['TEMP'] == '-999.000' :
          TEMP_C0G640 = TEMP_C0G640
       else :
-         if (float(target_data[i]['TEMP']) > TEMP_C0G640):
+         if float(target_data[i]['TEMP']) > TEMP_C0G640:
             TEMP_C0G640 = float(target_data[i]['TEMP'])
-   elif (target_data[i]['station_id'] == 'C0R190') :
-      if (target_data[i]['TEMP'] == '-99.000' or target_data[i]['TEMP'] == '-999.000') :
+   elif target_data[i]['station_id'] == 'C0R190' :
+      if target_data[i]['TEMP'] == '-99.000' or target_data[i]['TEMP'] == '-999.000' :
          TEMP_C0R190 = TEMP_C0R190
       else :
-         if (float(target_data[i]['TEMP']) > TEMP_C0R190):
+         if float(target_data[i]['TEMP']) > TEMP_C0R190:
             TEMP_C0R190 = float(target_data[i]['TEMP'])
-   elif (target_data[i]['station_id'] == 'C0X260') :
-      if (target_data[i]['TEMP'] == '-99.000' or target_data[i]['TEMP'] == '-999.000') :
+   elif target_data[i]['station_id'] == 'C0X260' :
+      if target_data[i]['TEMP'] == '-99.000' or target_data[i]['TEMP'] == '-999.000' :
          TEMP_C0X260 = TEMP_C0X260
       else :
-         if (float(target_data[i]['TEMP']) > TEMP_C0X260):
+         if float(target_data[i]['TEMP']) > TEMP_C0X260:
             TEMP_C0X260 = float(target_data[i]['TEMP'])
-if (TEMP_C0A880 == 0) :
+if TEMP_C0A880 == 0 :
    val_C0A880 = str(None)
 else :
-   val_C0A880 = str(format(TEMP_C0A880, '.2f'))
-if (TEMP_C0F9A0 == 0) :
+   val_C0A880 = TEMP_C0A880
+if TEMP_C0F9A0 == 0 :
    val_C0F9A0 = str(None)
 else :
-   val_C0F9A0 = str(format(TEMP_C0F9A0, '.2f'))
-if (TEMP_C0G640 == 0) :
+   val_C0F9A0 = TEMP_C0F9A0
+if TEMP_C0G640 == 0 :
    val_C0G640 = str(None)
 else :
-   val_C0G640 = str(format(TEMP_C0G640, '.2f'))
-if (TEMP_C0R190 == 0) :
+   val_C0G640 = TEMP_C0G640
+if TEMP_C0R190 == 0 :
    val_C0R190 = str(None)
 else :
-   val_C0R190 = str(format(TEMP_C0R190, '.2f'))
-if (TEMP_C0X260 == 0) :
+   val_C0R190 = TEMP_C0R190
+if TEMP_C0X260 == 0 :
    val_C0X260 = str(None)
 else :
-   val_C0X260 = str(format(TEMP_C0X260, '.2f'))
+   val_C0X260 = TEMP_C0X260
 
 
-print('''[['C0A880',''', val_C0A880 + '],', '''['C0F9A0',''', val_C0F9A0 + '],', '''['C0G640',''', val_C0G640 + '],', '''['C0R190',''', val_C0R190 + '],', '''['C0X260',''', val_C0X260 + ']]')  
+print("[['C0A880',", val_C0A880, "],", "['C0F9A0',", val_C0F9A0, "],", "['C0G640',", val_C0G640, "],", "['C0R190',", val_C0R190, "],", "['C0X260',", val_C0X260,"]]")
